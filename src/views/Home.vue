41a71669c6b89923fbed9cb81f064452f58e50b2
NewThis1996 <702381450@qq.com>
2020-08-20 08:29:04 +0800

    尝试提交cli

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+      <div class="con-header">
+        <img  class="con-header-logo" src="../assets/light_chrome.png" width="40px" height="40px" >
+        <span class="con-header-title">极简插件</span>
+        <div class="demo-input-suffix">
+        <el-input placeholder="请输入内容" prefix-icon="el-icon-search">
+        </el-input>
+</div>
+      </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: '',
+    data() {
+      return {
+        
+      }
+    }
   }
-}
 </script>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  .con-header{
+    width: 100%;
+    height: 64px;
+    background-color: #2C3742;
+    /* vertical-align:-5px; */
+    /* display: block; */
+    position: relative;
+  }
+  .con-header-title{
+    font-size: 30px;
+    line-height: 64px;
+    text-align: center;
+    color: #ffffff;
+    margin-left: 26px;
+  }
+  .con-header-logo{
+    position:relative;
+    top: 10px;
+    margin-left: 219px;
+  }
+  .el-input__inner{
+    width: 100px;
+    display: inline-block;
+    width: 276px; 
+    height: 37px;
+  }
+</style>
